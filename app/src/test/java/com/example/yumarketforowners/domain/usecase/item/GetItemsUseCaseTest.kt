01d240a22e0fbb66afc9438825e964138f432277
ErroredPasta <ForeignServices3132@pm.me>
2022-11-03 02:30:29 +0900

    GetItemsUseCaseTest.kt 주석 처리

--- app/src/test/java/com/example/yumarketforowners/domain/usecase/item/GetItemsUseCaseTest.kt
+++ app/src/test/java/com/example/yumarketforowners/domain/usecase/item/GetItemsUseCaseTest.kt
@@ -1,47 +1,47 @@
 package com.example.yumarketforowners.domain.usecase.item
-
-import com.example.yumarketforowners.data.repository.FakeItemRepository
-import com.example.yumarketforowners.domain.repository.ItemRepository
-import com.google.common.truth.Truth.assertThat
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.runTest
-import org.junit.Before
-import org.junit.Test
-
-@ExperimentalCoroutinesApi
-class GetItemsUseCaseTest {
-    private lateinit var sut: GetItemsUseCase
-
-    // region test doubles =========================================================================
-    private lateinit var fakeRepository: ItemRepository
-    // endregion test doubles ======================================================================
-
-    @Before
-    fun setup() {
-        fakeRepository = FakeItemRepository()
-
-        sut = GetItemsUseCase(fakeRepository)
-    }
-
-    @Test
-    fun `get available items`() = runTest {
-        // act
-        val result = sut(marketId = 0L, available = true)!!
-
-        // assert
-        for (item in result) {
-            assertThat(item.available).isTrue()
-        }
-    }
-
-    @Test
-    fun `get unavailable items`() = runTest {
-        // act
-        val result = sut(marketId = 0L, available = false)!!
-
-        // assert
-        for (item in result) {
-            assertThat(item.available).isFalse()
-        }
-    }
-}
+//
+//import com.example.yumarketforowners.data.repository.FakeItemRepository
+//import com.example.yumarketforowners.domain.repository.ItemRepository
+//import com.google.common.truth.Truth.assertThat
+//import kotlinx.coroutines.ExperimentalCoroutinesApi
+//import kotlinx.coroutines.test.runTest
+//import org.junit.Before
+//import org.junit.Test
+//
+//@ExperimentalCoroutinesApi
+//class GetItemsUseCaseTest {
+//    private lateinit var sut: GetItemsUseCase
+//
+//    // region test doubles =========================================================================
+//    private lateinit var fakeRepository: ItemRepository
+//    // endregion test doubles ======================================================================
+//
+//    @Before
+//    fun setup() {
+//        fakeRepository = FakeItemRepository()
+//
+//        sut = GetItemsUseCase(fakeRepository)
+//    }
+//
+//    @Test
+//    fun `get available items`() = runTest {
+//        // act
+//        val result = sut(marketId = 0L, available = true)!!
+//
+//        // assert
+//        for (item in result) {
+//            assertThat(item.available).isTrue()
+//        }
+//    }
+//
+//    @Test
+//    fun `get unavailable items`() = runTest {
+//        // act
+//        val result = sut(marketId = 0L, available = false)
+//
+//        // assert
+//        for (item in result) {
+//            assertThat(item.available).isFalse()
+//        }
+//    }
+//}
