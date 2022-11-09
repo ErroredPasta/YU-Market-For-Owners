0211986092cfca548d19b726366c7ba23ef3fa02
ErroredPasta <ForeignServices3132@pm.me>
2022-11-10 04:02:39 +0900

    ImageView.load에 placeholder 추가

--- app/src/main/java/com/example/yumarketforowners/presentation/extension/ImageViewExtension.kt
+++ app/src/main/java/com/example/yumarketforowners/presentation/extension/ImageViewExtension.kt
@@ -2,6 +2,7 @@ package com.example.yumarketforowners.presentation.extension
 
 import android.graphics.Bitmap
 import android.widget.ImageView
+import androidx.annotation.DrawableRes
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.Transformation
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -9,6 +10,7 @@ import com.bumptech.glide.load.resource.bitmap.CenterInside
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
+import com.example.yumarketforowners.R
 
 // fade-in, fade-out의 애니메이션을 처리하기 위한 factory
 private val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
@@ -18,9 +20,11 @@ fun ImageView.clear() = Glide.with(this).clear(this)
 fun ImageView.load(
     uri: String,
     corner: Float = 0f,
-    scaleType: Transformation<Bitmap> = CenterInside()
+    scaleType: Transformation<Bitmap> = CenterInside(),
+    @DrawableRes placeholder: Int = R.drawable.ic_baseline_add_a_photo_24
 ) = Glide.with(this)
     .load(uri)
+    .placeholder(placeholder)
     .transition(DrawableTransitionOptions.withCrossFade(factory)) // fade-in 애니메이션
     .diskCacheStrategy(DiskCacheStrategy.ALL)
     .apply {
